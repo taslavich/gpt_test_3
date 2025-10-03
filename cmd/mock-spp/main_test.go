169601f38227f8482f17fe90cf4ab19a21bc1a34
Taslav <taslavbel@bk.ru>
2025-10-03 22:03:48 +0300

    Fix #46

--- cmd/mock-spp/main_test.go
+++ cmd/mock-spp/main_test.go
@@ -24,7 +24,7 @@ import (
 
 // Конфигурация теста
 const (
-	sppAdapterURL = "https://twinbidexchange.com/bidRequest/bid_v_2_5"
+	sppAdapterURL = "https://127.0.0.1:8086/bid_v_2_5"
 	threads       = 100              // Количество параллельных горутин
 	targetRPS     = 10000            // Целевая нагрузка (RPS)
 	testDuration  = 10 * time.Second // Длительность теста
