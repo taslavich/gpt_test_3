1dce94efacf2bdbbc893c58a4f24b11eae028a04
Taslav <you@example.com>
2025-10-15 12:46:35 +0300

    ff

--- cmd/mock-spp/main_test.go
+++ cmd/mock-spp/main_test.go
@@ -25,9 +25,9 @@ import (
 // Конфигурация теста
 const (
 	sppAdapterURL = "https://twinbidexchange.com/bidRequest/bid_v_2_5"
-	threads       = 100              // Количество параллельных горутин
-	targetRPS     = 10000            // Целевая нагрузка (RPS)
-	testDuration  = 10 * time.Second // Длительность теста
+	threads       = 100             // Количество параллельных горутин
+	targetRPS     = 10000           // Целевая нагрузка (RPS)
+	testDuration  = 2 * time.Second // Длительность теста
 )
 
 var (
