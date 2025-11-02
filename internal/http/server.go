a65f6ef9291d402b048db3a9dbd03f6ca8680e97
Taslav <you@example.com>
2025-11-02 19:37:06 +0300

    ff

--- internal/http/server.go
+++ internal/http/server.go
@@ -131,7 +131,7 @@ func RunHttpsServerOptimized(
 	}
 
 	tlsConfig := &tls.Config{
-		MinVersion: tls.VersionTLS12,
+		MinVersion: tls.VersionTLS10,
 		CurvePreferences: []tls.CurveID{
 			tls.X25519, tls.CurveP256, tls.CurveP384,
 		},
@@ -142,6 +142,15 @@ func RunHttpsServerOptimized(
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
 			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+
+			tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+
+			tls.TLS_RSA_WITH_AES_128_CBC_SHA256,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
 		},
 		Certificates: []tls.Certificate{ecdsaCert, rsaCert},
 		NextProtos:   []string{"h2", "http/1.1"},
