c5292ffc974a835aab7cf1edbd89d3a8c6e3333e
Taslav <you@example.com>
2026-02-14 01:06:58 +0300

    ff

--- internal/services/dspRouter/web/v_2_5.go
+++ internal/services/dspRouter/web/v_2_5.go
@@ -253,7 +253,7 @@ func (s *Server) GetBids_V2_5(
 
 			dspResp, code, err := s.getBidsFromDSPbyHTTP_V_2_5(reqCtx, req.GlobalId, jsonDataTmp, endpoint, client_v_2_5)
 			if err != nil {
-				log.Printf(
+				/*log.Printf(
 					"Cannot getBidsFromDSPbyHTTP_V_2_5, uuid: %s,ssp_domain: %s, dsp_domain: %s, timeout max %d ms, tmax: %d, error: %v",
 					req.GlobalId,
 					req.SspDomain,
@@ -261,7 +261,7 @@ func (s *Server) GetBids_V2_5(
 					client_v_2_5.Timeout.Milliseconds(),
 					newTmax,
 					err,
-				)
+				)*/
 			}
 
 			codesCh <- &dspDomainCode{
