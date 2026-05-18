56c464ff04c2f6a0cbf28f7894676904cac7909f
Your Name <you@example.com>
2026-05-18 08:59:51 +0300

    f

--- internal/services/clickhouse-loader/createDb.go
+++ internal/services/clickhouse-loader/createDb.go
@@ -136,9 +136,9 @@ CREATE TABLE IF NOT EXISTS {db}.fact_impressions
     win_final_price        Float64,
     win_dsp_price          Float64,
 
-    win_cid                UUID,
-    win_crid               UUID,
-    win_user_id            UUID
+    win_cid                Nullable(String),
+    win_crid               Nullable(String),
+    win_user_id            Nullable(String)
 )
 ENGINE = MergeTree
 PARTITION BY toYYYYMMDD(event_date)
@@ -190,9 +190,9 @@ CREATE TABLE IF NOT EXISTS {db}.fact_clicks
     win_final_price   Float64,
     win_dsp_price     Float64,
 
-    win_cid           UUID,
-    win_crid          UUID,
-    win_user_id       UUID
+    win_cid           Nullable(String),
+    win_crid          Nullable(String),
+    win_user_id       Nullable(String)
 )
 ENGINE = MergeTree
 PARTITION BY toYYYYMMDD(event_date)
@@ -208,9 +208,9 @@ SETTINGS index_granularity = 8192;
 
 CREATE TABLE IF NOT EXISTS {db}.agg_stats
 (
-    win_user_id         UUID,
-    win_cid             UUID,
-    win_crid            UUID,
+    win_user_id         Nullable(String),
+    win_cid             Nullable(String),
+    win_crid            UUNullable(String)ID,
 
     event_date          Date,
 
