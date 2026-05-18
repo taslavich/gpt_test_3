291396071d2e144cbd509c70dca40fbea48b8e6c
Your Name <you@example.com>
2026-05-18 15:38:30 +0300

    f

--- internal/services/sspAdapter/web/v_2_5.go
+++ internal/services/sspAdapter/web/v_2_5.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"net/http"
 	"runtime/debug"
-	"sync/atomic"
 	"time"
 
 	"github.com/ggicci/httpin"
@@ -34,7 +33,8 @@ func checkRegion(countryISO string) error {
 }
 
 func shouldPass(counter *uint64) bool {
-	return atomic.AddUint64(counter, 1)%100 < 5
+	//return atomic.AddUint64(counter, 1)%100 < 5
+	return true
 }
 
 func postBid_V2_5(
