1829bd2228239629df712d2ee1e9c2480f5bb443
Your Name <you@example.com>
2026-06-11 20:26:15 +0300

    fff

--- internal/services/sspAdapter/web/common.go
+++ internal/services/sspAdapter/web/common.go
@@ -34,7 +34,7 @@ func getAdm(
 	if err := utils.WriteStringToRedis(ctx, redisClients, input.GlobalId, constants.EVENT_TIME_CLICKS_COLUMN, time.Now().UTC().Format("2006-01-02 15:04:05.000"), false); err != nil {
 		log.Printf("failed to WriteStringToRedis EVENT_TIME_CLICKS_COLUMN in getAdm: %v", err)
 		redisWriteErrorMonitor.Record(err)
-	} else if err := utils.AddUUIDToRedisSet(ctx, redisClients, redisSetClicks, input.GlobalId, true); err != nil {
+	} else if err := utils.AddUUIDToRedisSet(ctx, redisClients, redisSetClicks, input.GlobalId, false); err != nil {
 		log.Printf("failed to add click UUID to Redis set in getAdm: %v", err)
 		redisWriteErrorMonitor.Record(err)
 	}
@@ -62,7 +62,7 @@ func getNurl(
 	if err := utils.WriteStringToRedis(ctx, redisClients, input.GlobalId, constants.EVENT_TIME_IMPRESSIONS_COLUMN, time.Now().UTC().Format("2006-01-02 15:04:05.000"), false); err != nil {
 		log.Printf("failed to WriteStringToRedis EVENT_TIME_IMPRESSIONS_COLUMN in getAdm: %v", err)
 		redisWriteErrorMonitor.Record(err)
-	} else if err := utils.AddUUIDToRedisSet(ctx, redisClients, redisSetImpressions, input.GlobalId, true); err != nil {
+	} else if err := utils.AddUUIDToRedisSet(ctx, redisClients, redisSetImpressions, input.GlobalId, false); err != nil {
 		log.Printf("failed to add impression UUID to Redis set in getNurl: %v", err)
 		redisWriteErrorMonitor.Record(err)
 	}
