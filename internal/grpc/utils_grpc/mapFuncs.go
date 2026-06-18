34d81622661737acb532feb61908092932160f8f
Your Name <you@example.com>
2026-06-18 21:35:54 +0300

    fff

--- internal/grpc/utils_grpc/mapFuncs.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-func ValuesSet(siteIdCommon map[string]string) map[string]struct{} {
-	domainCommonSet := make(map[string]struct{})
-	for _, v := range siteIdCommon {
-		domainCommonSet[v] = struct{}{}
-	}
-	return domainCommonSet
-}
